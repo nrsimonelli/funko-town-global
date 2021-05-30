fc30ff64e3eba8263afbb13e47bfd48845c91fc5
nrsimonelli <nrsimonelli@gmail.com>
2021-05-30 18:21:24 -0500

    cleaned dataset

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='footer-root h-32 w-full bg-primary-darkest'>
+    <div className='footer-root h-64 w-full bg-primary-darkest'>
       <div className='container mx-auto h-full flex-row justify-between items-center'>
         <div>Footer</div>
         <div>developed by me</div>
